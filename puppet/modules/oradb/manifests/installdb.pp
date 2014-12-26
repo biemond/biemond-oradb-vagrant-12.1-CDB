d62daecf8d54b88b16cb9c7b14aaa3e6875191a5
Edwin Biemond <biemond@gmail.com>
2014-12-26 17:50:21 +0100

    update modules

--- puppet/modules/oradb/manifests/installdb.pp
+++ puppet/modules/oradb/manifests/installdb.pp
@@ -30,6 +30,14 @@ define oradb::installdb(
 )
 {
 
+  if ( $createUser == true ){
+    fail("createUser parameter on installdb ${title} is removed from this oradb module, you need to create the oracle user and its groups yourself")
+  }
+
+  if ( $createUser == false ){
+    notify {"createUser parameter on installdb ${title} can be removed, createUser feature is removed from this oradb module":}
+  }
+
   if (!( $version in ['11.2.0.1','12.1.0.1','12.1.0.2','11.2.0.3','11.2.0.4'])){
     fail('Unrecognized database install version, use 11.2.0.1|11.2.0.3|11.2.0.4|12.1.0.1|12.1.0.1')
   }
@@ -83,13 +91,9 @@ define oradb::installdb(
       oracle_base_home_dir => $oracleBase,
       ora_inventory_dir    => $oraInventory,
       os_user              => $user,
-      os_group             => $group,
       os_group_install     => $group_install,
-      os_group_oper        => $group_oper,
       download_dir         => $downloadDir,
       log_output           => true,
-      user_base_dir        => $userBaseDir,
-      create_user          => $createUser,
     }
 
     if ( $zipExtract ) {
